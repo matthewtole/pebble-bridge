9f5453734db397f91bd2cee367908f061c24933a
Matthew Tole <matthewtole@gmail.com>
2014-02-28 17:38:10 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var program = require('commander');
+var io = require('socket.io-client');
+
+program
+  .version('0.0.1')
+  .option('-i, --id [id]', 'Bridge ID')
+  .parse(process.argv);
+
+var bridgeId = program.id;
+var verbose = program.verbose;
+
+var socket = io.connect('http://pblweb.com/bridge');
+socket.on('connect', function () {
+
+  socket.emit('id', { id: bridgeId });
+  console.log('Connected to Pebble Bridge with ID ' + bridgeId)
+
+  var server = http.createServer(function (req, res) {
+    res.writeHead(404).end();
+  });
+
+  server.listen(9000, function () {
+    var ws = new WebSocketServer({
+      httpServer: server
+    });
+
+    ws.on('request', function (request) {
+      var connection = request.accept();
+      connection.on('message', function (message) {
+        socket.emit('message', message);
+      });
+
+      socket.on('message', function (data) {
+        connection.sendBytes(new Buffer(Object.keys(data).map(function (k) { return data[k]; })));
+      });
+
+    });
+
+  });
+
+
+});
+
+socket.on('error', function (err) {
+  console.log('Could not connect to Pebble Bridge.');
+});
